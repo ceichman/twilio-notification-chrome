8a56b76598e2e3c7b1bff191ac66c6033b710dd8
Charlotte Eichman <c.eichman@wustl.edu>
2024-04-25 00:00:56 -0500

    broken task frame query selector on actual portal

--- content-script.js
+++ content-script.js
@@ -2,7 +2,7 @@
  * matches string for manifest.json.content-scripts[0].matches
  * "matches": [ "*://flex.twilio.com/agent-desktop/" ]
  *
- * html class of panel1: Twilio-AgentDesktopView.Panel1
+ * html class of panel1: Twilio-Splitter-Pane
  * 
  * detect changes in DOM using MutationObserver: 
  * https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
@@ -38,13 +38,33 @@ const observeDOM = ( function() {
   }
 })();
 
-const tasksFrame = document.querySelector(".Twilio-AgentDesktopView.Panel1");
+let tasksFrame = document.querySelector(".Twilio-Splitter-Pane");
+const resetTasksFrame = () => {
+    if ((!tasksFrame) || (JSON.stringify(tasksFrame) === "{}") ) {
+        // if the selector missed
+        console.log("frame selector missed on: ", JSON.stringify(tasksFrame));
+        setTimeout(() => {
+            tasksFrame = document.querySelector(".Twilio-Splitter-Pane");
+            resetTasksFrame();
+        }, 100);
+    }
+    else {
+        // if it hit
+        console.log("got the task frame: " + JSON.stringify(tasksFrame))
+    }
+}
+resetTasksFrame();
+
 const sound = new Audio(chrome.runtime.getURL('sounds/sound.mp3'));
 
 observeDOM(tasksFrame, function(mutations) {
+    if ((!tasksFrame) || (tasksFrame === {}) ) {
+        tasksFrame = document.querySelector(".Twilio-Splitter-Pane");
+    }
     for (record of mutations) {
         if (record.addedNodes.length) {
             // play sound, or do some other debug thing
+            alert("dom change located")
             sound.play();
             break;
         }
